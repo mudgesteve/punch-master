88d55bc330820b2afa4233a886c0aa777e48adff
Steve Mudge <mudge.steve@gmail.com>
2020-12-12 16:26:46 -0800

    Addig punchMaster

---
+++ pages/DashBoard.js
@@ -0,0 +1,10 @@
+
+import styles from '../styles/Home.module.css'
+
+export default function DashBoard() {
+  return (
+    <div className={styles.container}>
+        <h1>Punch Master</h1>
+    </div>
+  )
+}
